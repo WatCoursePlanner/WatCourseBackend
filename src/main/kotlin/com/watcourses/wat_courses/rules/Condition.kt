dcfc0bdfd2e00bab6959dc9e01b696091736e635
Harry Yu <harryyunull@gmail.com>
2020-07-06 13:00:18 -0400

    parsing requisite rules

--- src/main/kotlin/com/watcourses/wat_courses/rules/Condition.kt
+++ src/main/kotlin/com/watcourses/wat_courses/rules/Condition.kt
@@ -2,10 +2,67 @@ package com.watcourses.wat_courses.rules
 
 enum class ConditionType { TRUE, FALSE, AND, OR, NOT, HAS_COURSE }
 
-data class Condition(val type: ConditionType, val operands: List<Condition>) {
+data class Condition(val type: ConditionType, val operands: List<Condition>, val data: String? = null) {
+    class ParseFailure(str: String, reason: String) : Exception("Failed to parse $str because $reason")
     companion object {
-        fun parseFromText(text: String): Condition {
-            return Condition(ConditionType.TRUE, emptyList())
+        // resolve "123" in CS 101, 123 to course("CS 123")
+        private fun resolveCourse(raw: String, parts: List<String>, index: Int): Condition {
+            val part = parts[index]
+            if (part.contains(" ")) // e.g. CS 101. We have "CS" already so return directly
+                return course(part)
+            // look backwards for identifier
+            val identifier = parts.subList(0, index).findLast { it.contains(" ") }?.substringBefore(" ")
+                ?: ParseFailure(raw, "Can't find a course identifier")
+            return course("$identifier $part")
         }
+
+        /*
+         * Example: CS 101, 123, CS 102/ECE 123, CS 233 => AND(CS101, CS123, OR(CS102, ECE123), CS233)
+         */
+        fun parseFromText(text: String): Condition {
+            val processedText = text.substringAfter(":").trim()
+            val andParts = processedText.split(",").map { it.trim() }
+            return Condition(ConditionType.AND, andParts.mapIndexed { i, part ->
+                if (part.contains("/")) {
+                    val orParts = part.split("/")
+                    Condition(ConditionType.OR, orParts.mapIndexed { j, _ -> resolveCourse(text, orParts, j) })
+                } else
+                    resolveCourse(text, andParts, i)
+            })
+        }
+
+        fun and(vararg conditions: Condition) = Condition(ConditionType.AND, conditions.toList())
+        fun or(vararg conditions: Condition) = Condition(ConditionType.OR, conditions.toList())
+        fun not(condition: Condition) = Condition(ConditionType.NOT, listOf(condition))
+        fun alwaysTrue() = Condition(ConditionType.TRUE, listOf())
+        fun alwaysFalse() = Condition(ConditionType.FALSE, listOf())
+        fun course(code: String) = Condition(ConditionType.HAS_COURSE, listOf(), data = code)
+    }
+
+    fun check(courseCodeSet: Set<String>): Boolean {
+        return when (type) {
+            ConditionType.TRUE -> true
+            ConditionType.FALSE -> false
+            ConditionType.AND -> operands.map { it.check(courseCodeSet) }.reduce { a, b -> a && b }
+            ConditionType.OR -> operands.map { it.check(courseCodeSet) }.reduce { a, b -> a || b }
+            ConditionType.NOT -> !operands.single().check(courseCodeSet)
+            ConditionType.HAS_COURSE -> courseCodeSet.contains(data)
+        }
+    }
+
+    private fun toStringInternal(): String {
+        val bracketIfComplex = { c: String -> if (c.contains("&&") || c.contains("||")) "($c)" else c }
+        return when (type) {
+            ConditionType.TRUE -> "true"
+            ConditionType.FALSE -> "false"
+            ConditionType.AND -> bracketIfComplex(operands.joinToString(" && ") { it.toStringInternal() })
+            ConditionType.OR -> bracketIfComplex(operands.joinToString(" || ") { it.toStringInternal() })
+            ConditionType.NOT -> "!${operands.single().toStringInternal()}"
+            ConditionType.HAS_COURSE -> "$data"
+        }
+    }
+
+    override fun toString(): String {
+        return toStringInternal().trimStart('(').trimEnd(')')
     }
 }
--- src/test/kotlin/com/watcourses/wat_courses/RuleTests.kt
+++ src/test/kotlin/com/watcourses/wat_courses/RuleTests.kt
@@ -1,10 +1,56 @@
 package com.watcourses.wat_courses
 
+import com.watcourses.wat_courses.rules.Condition
+import com.watcourses.wat_courses.rules.Condition.Companion.alwaysFalse
+import com.watcourses.wat_courses.rules.Condition.Companion.alwaysTrue
+import com.watcourses.wat_courses.rules.Condition.Companion.and
+import com.watcourses.wat_courses.rules.Condition.Companion.course
+import com.watcourses.wat_courses.rules.Condition.Companion.not
+import com.watcourses.wat_courses.rules.Condition.Companion.or
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class RuleTests {
     @Test
-    fun `parsing works`() {
+    fun `checking works`() {
+        val testSet = setOf("A", "B", "D")
+        assertThat(alwaysTrue().check(emptySet())).isTrue()
+        assertThat(alwaysFalse().check(emptySet())).isFalse()
+        assertThat(course("A").check(testSet)).isTrue()
+        assertThat(course("C").check(testSet)).isFalse()
+        assertThat(and(course("A"), course("B")).check(testSet)).isTrue()
+        assertThat(and(course("A"), course("B"), alwaysTrue()).check(testSet)).isTrue()
+        assertThat(and(course("A"), course("B"), alwaysFalse()).check(testSet)).isFalse()
+        assertThat(and(course("A"), course("C")).check(testSet)).isFalse()
+        assertThat(or(course("A"), course("C")).check(testSet)).isTrue()
+        assertThat(and(course("A"), not(course("C"))).check(testSet)).isTrue()
+        assertThat(and(course("A"), not(course("A"))).check(testSet)).isFalse()
+        assertThat(and(course("A"), not(and(course("A"), course("C")))).check(testSet)).isTrue()
+        assertThat(and(course("A"), not(and(course("A"), course("D")))).check(testSet)).isFalse()
+    }
 
+    @Test
+    fun `parsing works`() {
+        val condition =
+            Condition.parseFromText(
+                "req: BME 121, CS 115, 135, 137, CHE 121, MTE 121/GENE 121, NE 111, MSCI 121"
+            )
+        assertThat(condition.toString())
+            .isEqualTo("BME 121 && CS 115 && CS 135 && CS 137 && CHE 121 && (MTE 121 || GENE 121) && NE 111 && MSCI 121")
+        assertThat(
+            condition.check(
+                setOf("BME 121", "CS 115", "CS 135", "CS 137", "CHE 121", "NE 111", "MSCI 121", "MTE 121", "EXTRA 100")
+            )
+        ).isTrue()
+        assertThat(
+            condition.check(
+                setOf("BME 121", "CS 115", "CS 135", "CS 137", "CHE 121", "NE 111", "MSCI 121", "GENE 121", "EXTRA 100")
+            )
+        ).isTrue()
+        assertThat(
+            condition.check(
+                setOf("BME 121", "CS 115", "CS 137", "CHE 121", "NE 111", "MSCI 121", "MTE 121", "EXTRA 100")
+            )
+        ).isFalse()
     }
 }
