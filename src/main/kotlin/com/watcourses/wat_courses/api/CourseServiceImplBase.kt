197935cdf37330b5bdbb479a024d38a3eafc07ca
CarbonylGroup <carbonylgp@gmail.com>
2020-07-12 13:21:09 -0400

    manually create ImplBase

---
+++ src/main/kotlin/com/watcourses/wat_courses/api/CourseServiceImplBase.kt
@@ -0,0 +1,113 @@
+package com.watcourses.wat_courses.api
+
+import com.watcourses.wat_courses.proto.CourseServiceGrpc
+import com.watcourses.wat_courses.proto.*
+import io.grpc.*
+import io.grpc.stub.ServerCalls
+import io.grpc.stub.StreamObserver
+import io.rouz.grpc.ContextCoroutineContextElement
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+abstract class CourseServiceImplBase(
+        coroutineContext: CoroutineContext = Dispatchers.Default
+) : BindableService, CoroutineScope {
+
+    private val _coroutineContext: CoroutineContext = coroutineContext
+
+    override val coroutineContext: CoroutineContext
+        get() = ContextCoroutineContextElement() + _coroutineContext
+
+    open suspend fun getCourseInfo(request: com.watcourses.wat_courses.proto.CourseInfoRequest): com.watcourses.wat_courses.proto.CourseInfo {
+        throw unimplemented(CourseServiceGrpc.getGetCourseInfoMethod()).asRuntimeException()
+    }
+
+    internal fun getCourseInfoInternal(
+            request: com.watcourses.wat_courses.proto.CourseInfoRequest,
+            responseObserver: StreamObserver<CourseInfo>
+    ) {
+        launch {
+            tryCatchingStatus(responseObserver) {
+                val response = getCourseInfo(request)
+                onNext(response)
+            }
+        }
+    }
+
+    override fun bindService(): ServerServiceDefinition {
+        return ServerServiceDefinition.builder(CourseServiceGrpc.getServiceDescriptor())
+                .addMethod(
+                        CourseServiceGrpc.getGetCourseInfoMethod(),
+                        ServerCalls.asyncUnaryCall(
+                                MethodHandlers(METHODID_GET_COURSE_INFO)
+                        )
+                )
+                .build()
+    }
+
+    private fun unimplemented(methodDescriptor: MethodDescriptor<*, *>): Status {
+        return Status.UNIMPLEMENTED
+                .withDescription("Method ${methodDescriptor.fullMethodName} is unimplemented")
+    }
+
+    private fun <E> handleException(t: Throwable?, responseObserver: StreamObserver<E>) {
+        when (t) {
+            null -> return
+            is CancellationException -> handleException(t.cause, responseObserver)
+            is StatusException, is StatusRuntimeException -> responseObserver.onError(t)
+            is RuntimeException -> {
+                responseObserver.onError(Status.UNKNOWN.asRuntimeException())
+                throw t
+            }
+            is Exception -> {
+                responseObserver.onError(Status.UNKNOWN.asException())
+                throw t
+            }
+            else -> {
+                responseObserver.onError(Status.INTERNAL.asException())
+                throw t
+            }
+        }
+    }
+
+    private suspend fun <E> tryCatchingStatus(responseObserver: StreamObserver<E>, body: suspend StreamObserver<E>.() -> Unit) {
+        try {
+            responseObserver.body()
+            responseObserver.onCompleted()
+        } catch (t: Throwable) {
+            handleException(t, responseObserver)
+        }
+    }
+
+    private val METHODID_GET_COURSE_INFO = 0
+
+    private inner class MethodHandlers<Req, Resp> internal constructor(
+            private val methodId: Int
+    ) : ServerCalls.UnaryMethod<Req, Resp>,
+            ServerCalls.ServerStreamingMethod<Req, Resp>,
+            ServerCalls.ClientStreamingMethod<Req, Resp>,
+            ServerCalls.BidiStreamingMethod<Req, Resp> {
+
+        @Suppress("UNCHECKED_CAST")
+        override fun invoke(request: Req, responseObserver: StreamObserver<Resp>) {
+            when (methodId) {
+                METHODID_GET_COURSE_INFO ->
+                    this@CourseServiceImplBase.getCourseInfoInternal(
+                            request as com.watcourses.wat_courses.proto.CourseInfoRequest,
+                            responseObserver as StreamObserver<CourseInfo>
+                    )
+                else -> throw AssertionError()
+            }
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        override fun invoke(responseObserver: StreamObserver<Resp>): StreamObserver<Req> {
+            when (methodId) {
+                else -> throw AssertionError()
+            }
+        }
+    }
+}
--- src/main/kotlin/com/watcourses/wat_courses/api/GreetingService.kt
+++ src/main/kotlin/com/watcourses/wat_courses/api/GreetingService.kt
@@ -1,11 +1,11 @@
 package com.watcourses.wat_courses.api
 
 import com.watcourses.wat_courses.persistence.DbCourseRepo
-import com.watcourses.wat_courses.proto.CourseServiceImplBase
-import com.watcourses.wat_courses.proto.Grpc.CourseInfoRequest
-import com.watcourses.wat_courses.proto.Courses.*
+import com.watcourses.wat_courses.proto.CourseInfoRequest
+import com.watcourses.wat_courses.proto.CourseInfo
 import kotlinx.coroutines.*
 import org.lognet.springboot.grpc.GRpcService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 import java.util.concurrent.Executors.newFixedThreadPool
@@ -15,8 +15,10 @@ class CourseService : CourseServiceImplBase(
         coroutineContext = newFixedThreadPool(4).asCoroutineDispatcher()
 ) {
 
+    @Autowired
+    private lateinit var dbCourseRepo: DbCourseRepo
+
     override suspend fun getCourseInfo(request: CourseInfoRequest): CourseInfo {
-        val dbCourseRepo: DbCourseRepo
         return dbCourseRepo.findByCode(request.code)!!.toProto()
     }
 }
