fdac8e106777a25d08882f411853179cf3c445c7
Harry Yu <harryyunull@gmail.com>
2020-07-20 17:34:50 -0400

    CORS

--- src/main/kotlin/com/watcourses/wat_courses/WatCoursesApplication.kt
+++ src/main/kotlin/com/watcourses/wat_courses/WatCoursesApplication.kt
@@ -2,10 +2,23 @@ package com.watcourses.wat_courses
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class WatCoursesApplication
+class WatCoursesApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<WatCoursesApplication>(*args)
+    runApplication<WatCoursesApplication>(*args)
 }
